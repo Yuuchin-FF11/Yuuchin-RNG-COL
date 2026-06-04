863ca1cf69939012290ee01599c007bfeca9de66
Yuuchin-FF11 <yuuchin@example.com>
2026-06-04 17:37:36 +0900

    Switch local server startup script default browser to msedge.exe

--- control-center/ローカルサーバー起動.bat
+++ control-center/ローカルサーバー起動.bat
@@ -1,6 +1,6 @@
 @echo off
 for /f "tokens=5" %%a in ('netstat -aon ^| findstr ":8080" ^| findstr "LISTENING"') do taskkill /f /pid %%a 2>nul
 cd /d "%~dp0.."
-start chrome.exe --new-window "http://localhost:8080/control-center/index.html"
+start msedge.exe --new-window "http://localhost:8080/control-center/index.html"
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -File server.ps1
 pause

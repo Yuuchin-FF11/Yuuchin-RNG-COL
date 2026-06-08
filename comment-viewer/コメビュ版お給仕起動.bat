44d9db2a2e16daa7e684177559ca5c8055d96510
Yuuchin-FF11 <yuuchin@example.com>
2026-06-08 17:12:17 +0900

    キーボード操作解説記事の完成とハイライト画像素材のピクセルパーフェクト化

--- comment-viewer/コメビュ版お給仕起動.bat
+++ comment-viewer/コメビュ版お給仕起動.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0.."
-start msedge.exe --disable-web-security --disable-site-isolation-trials --user-data-dir="%temp%\edge_translation_app" --use-fake-ui-for-media-stream --app="http://localhost:8080/comment-viewer/admin.html"
+start msedge.exe --disable-web-security --disable-site-isolation-trials --user-data-dir="%temp%\edge_translation_app" --use-fake-ui-for-media-stream "http://localhost:8080/comment-viewer/admin.html"
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -File server.ps1
 pause
